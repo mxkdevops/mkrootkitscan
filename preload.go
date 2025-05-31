d1c26e255fd4a267dc8b3d58cefd363a0b312de3
Mohammad <136272255+mxkdevops@users.noreply.github.com>
2025-05-31 12:23:20 +0100

    Create preload.go

---
+++ preload.go
@@ -0,0 +1,7 @@
+func ScanLDPreload() string {
+    content, err := ioutil.ReadFile("/etc/ld.so.preload")
+    if err == nil && strings.TrimSpace(string(content)) != "" {
+        return "⚠️ Non-empty /etc/ld.so.preload: " + string(content)
+    }
+    return ""
+}
